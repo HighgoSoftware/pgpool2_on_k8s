241fc9a9063d748fbffa773d8a0ab594b34b7d7d
Cary Huang <cary.huang@highgo.ca>
2023-09-19 11:41:41 -0700

    add proxy build

--- pgpool.docker/build.sh
+++ pgpool.docker/build.sh
@@ -1,4 +1,5 @@
 docker build --no-cache --build-arg PGPOOL_VER=4.4.3 --build-arg PGPOOL_BRANCH=hg-private-deploy -f Dockerfile.pgpool-release -t hgneon/pgpool:v4.4.3 .
+docker build --no-cache --build-arg GIT_USERNAME=$1 --build-arg GIT_PASSWORD=$2 --build-arg PGPOOL_VER=4.4.3 --build-arg PGPOOL_BRANCH=hg-private-deploy -f Dockerfile.proxy-release -t hgneon/proxy:v4.4.3 .
 
 ## run below command manually for more build information
 #docker build --no-cache --build-arg PGPOOL_VER=4.4.3 --build-arg PGPOOL_BRANCH=hg-private-deploy -f Dockerfile.pgpool -t hgneon/pgpool:v4.4.3 . --progress plain
